3b59e3b00d979d0c0e33e845c927004c56cb5fe3
AlexandrStegnin <alexandr.stegnin@gmail.com>
2018-11-14 13:44:44 +0500

    ДЗ к 5 уроку Архитектуры и шаблоны проектирования.

--- src/test/java/ru/stegnin/javaArchitectureHW/JavaArchitectureHwApplicationTests.java
+++ src/test/java/ru/stegnin/javaArchitectureHW/JavaArchitectureHwApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.stegnin.javaArchitectureHW.HW1.builder.Builder;
 import ru.stegnin.javaArchitectureHW.HW1.entity.User;
+import ru.stegnin.javaArchitectureHW.HW3.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,6 +24,17 @@ public class JavaArchitectureHwApplicationTests {
                 .with(u -> u.setPassword("admin"))
                 .get();
         Assert.assertEquals(user.getLogin(), "Admin");
+    }
+
+    @Test
+    public void testCheckUser() {
+        Server server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+        Middleware middleware = new UserExistsMiddleware(server);
+        middleware.linkWith(new RoleCheckMiddleware());
+        server.setMiddleware(middleware);
+        Assert.assertTrue(server.logIn("admin@example.com", "admin_pass"));
     }
 
 }
